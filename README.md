a35ccf515f951b319ca2d12212bf949dfb79d710
Leigh Hunt <leigh@venari.co.nz>
2020-05-14 15:23:32 +1200

    Date loop

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # days-since-each-nz-dhb-had-covid-case
+
+- Needs CoreUtils on mac to use Gnu date.
+
+```
+$ brew install coreutils
+```
---
+++ create-animation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Neads GNU gdate
+# brew install coreutils
+
+start='2020-02-25'      # Date of first case
+end=$(gdate --iso-8601) # Run to today...
+#end='2019-02-01'       # ... or set end date
+
+start=$(gdate -d $start +%Y%m%d)
+end=$(gdate -d $end +%Y%m%d)
+
+while [[ $start -le $end ]]
+do
+        echo $start
+        start=$(gdate -d"$start + 1 day" +"%Y%m%d")
+
+done
